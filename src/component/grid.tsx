f6d9167615470ade7aa8c71908f0c78258f77667
JW <jeongwoo_lee3@tmax.co.kr>
2023-06-11 17:46:46 +0900

    fix: grid 컴포넌트 async await 사용, undefiend일때는 렌더링 안하도록

--- src/component/grid.tsx
+++ src/component/grid.tsx
@@ -1,17 +1,18 @@
-import React from "react";
-import { AnimalList } from "../api/api";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import Card from './card'
+import { AnimalList } from "../api/api";
 import { AnimalInfo } from "../App";
 
 const ImageGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr); 
-  gap: 16px; 
+  grid-template-columns: repeat(3, 1fr);
+  gap: 16px;
 `;
 const GridContent = styled.div`
-	display: flex;
-	flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 const ImageItem = styled.img`
   width: 100%;
   height: auto;
@@ -19,16 +20,35 @@ const ImageItem = styled.img`
 
 const GridComponent = () => {
 
-    const animalList = AnimalList( 10,  1,  5).then()
-  
-    return (
-      <ImageGrid>
-				<GridContent></GridContent>
-        {animalList.map((animal, index) => (
-          <ImageItem key={index} src={animal.filePath} alt={`Image ${index + 1}`} />
-        ))}
-      </ImageGrid>
-    );
-  };
-  
-  export default GridComponent;
+  const [animalList, setAnimalList] = useState<AnimalInfo[]| undefined>(undefined); 
+  useEffect(() => {
+    const fetchData = async () => {
+      const list = await AnimalList(10, 1, 5);
+      setAnimalList(list)
+      // animalList을 사용하여 필요한 작업을 수행합니다.
+      // 예: 상태로 설정하거나, 컴포넌트 내부에서 사용합니다.
+    };
+
+    fetchData();
+  }, []);
+
+
+
+  return (
+    <ImageGrid>
+      
+      {animalList && animalList.map((animal, index) => (
+        <GridContent>
+          <Card
+            src={animal.filePath}
+            alt={`Image ${index + 1}`}
+            description={'gkdl..'}
+          />
+        </GridContent>
+      ))}
+      
+    </ImageGrid>
+  );
+};
+
+export default GridComponent;
